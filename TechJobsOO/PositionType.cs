0538d0a3731b32f688b97d914236de01655aea76
Grace Brown <gracenbrown@sbcglobal.net>
2020-12-08 13:09:55 -0600

    Completed supporting classes

--- TechJobsOO/PositionType.cs
+++ TechJobsOO/PositionType.cs
@@ -18,6 +18,20 @@ namespace TechJobsOO
             Value = value;
         }
 
-        // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is PositionType type &&
+                   Id == type.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
     }
 }
